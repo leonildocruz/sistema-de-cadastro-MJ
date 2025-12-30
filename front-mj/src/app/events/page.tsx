7be323151a7c2da7ea55d241fd3defcb7a84bad4
Leo Silva <leosilva.cruz93@gmail.com>
2025-12-29 22:02:37 -0300

    adicao de icones

---
+++ front-mj/src/app/events/page.tsx
@@ -0,0 +1,9 @@
+import { LayoutAdmin } from "@/components/layouts/LayoutAdmin";
+
+export default function EventsPage() {
+  return (
+    <LayoutAdmin>
+      <h1>Events Page</h1>
+    </LayoutAdmin>
+  );
+}
---
+++ front-mj/src/app/users/page.tsx
@@ -0,0 +1,9 @@
+import { LayoutAdmin } from "@/components/layouts/LayoutAdmin";
+
+export default function UsersPage() {
+  return (
+    <LayoutAdmin>
+      <h1>Users Page</h1>
+    </LayoutAdmin>
+  );
+}
