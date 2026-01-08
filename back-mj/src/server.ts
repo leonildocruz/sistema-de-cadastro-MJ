1893c944faea1e911fe51b8f11569f10a14e7c6f
nizael <nizael.dev@gmail.com>
2026-01-07 21:25:31 -0300

    feat: install cors

--- back-mj/src/server.ts
+++ back-mj/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { routes } from "./routes";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
